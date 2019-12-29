baad69eb8bf3e080538634604b596c9f3be63a8b
M'bark Erras <merras@e1r1p5.1337.ma>
2019-12-29 15:32:52 +0100

    changing list_copy() to receive a filter

--- srcs/simplist_copy.c
+++ srcs/simplist_copy.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simplist_copy.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merras <mbarekerras@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/18 21:04:17 by merras            #+#    #+#             */
+/*   Updated: 2019/12/29 15:32:21 by merras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "simplist.h"
 
-t_list	*list_copy(t_list *head)
+t_list	*list_copy(t_list *head, int (*filter)(t_list *))
 {
 	t_list	*copy;
 	void	*content;
@@ -8,9 +20,12 @@ t_list	*list_copy(t_list *head)
 	copy = NULL;
 	while (head)
 	{
-		content = malloc(head->content_size);
-		ft_memcpy(content, head->content, head->content_size);
-		list_push_back(&copy, list_create_node(content, head->content_size));
+		if (filter(head))
+		{
+			content = malloc(head->content_size);
+			ft_memcpy(content, head->content, head->content_size);
+			list_push_back(&copy, list_create_node(content, head->content_size));
+		}
 		head = head->next;
 	}
 	return (copy);
